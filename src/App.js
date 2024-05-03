9d80417c130fb05c3b2fea01b0f1c12d0179476c
yaho <rlatjwls2923@gmail.com>
2024-05-04 02:27:49 +0900

    feat: myPage

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./App.css";
 import { Header } from "./components/Header";
 import Info from "./ChildInfo";
+import myPage from "./MyPage";
 function App() {
   return (
     <>
-      <Info />
+      <myPage />
     </>
   );
 }
---
+++ src/MyPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function myPage() {
+  return <div>
+    
+  </div>;
+}
+
+export default myPage;
