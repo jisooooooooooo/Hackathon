1276b8008ba82504e1e5ec1533d081a846f8a991
jisooooooooooo <dlawltn0708@naver.com>
2024-05-04 00:32:17 +0900

    Feat: impot문 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
 import styles from "./Header.module.css";
 
-
 export const Header = () => {
   return (
     <div className={styles.container}>
-      <div>오늘의 식습관</div>
-      <div>캘린더</div>
-      <div>콘텐츠</div>
-      <div>컨설팅</div>
-      <div>마이페이지</div>
+      <div>로고</div>
+      <div className={styles.right}>
+        <div>오늘의 식습관</div>
+        <div>캘린더</div>
+        <div>콘텐츠</div>
+        <div>컨설팅</div>
+        <div>마이페이지</div>
+      </div>
     </div>
   );
 };
