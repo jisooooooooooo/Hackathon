154aec3ad838715df347cf029754e5f39f2d2f5c
yaho <rlatjwls2923@gmail.com>
2024-05-04 02:02:12 +0900

    feat: child info page complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Header } from "./components/Header";
+import Info from "./ChildInfo";
 function App() {
-  return <>
-  </>;
+  return (
+    <>
+      <Info />
+    </>
+  );
 }
 
 export default App;
---
+++ src/ChildInfo.js
@@ -0,0 +1,85 @@
+import React from "react";
+import "./ChildInfo.css";
+import { useState } from "react";
+
+function Info() {
+  const [name, setName] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const [gender, setGender] = useState("");
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [pastDisease, setPastDisease] = useState("");
+  const [extra, setExtra] = useState("");
+  const [info, setInfo] = useState([]);
+
+  const addInfo = () => {
+    setInfo([name, birthday, gender, height, weight, pastDisease, extra, info]);
+  };
+  
+  return (
+    <div className="info">
+      <h2>아이정보를 입력해주세요</h2>
+      <div className="info_div">
+        <h4 className="info_title">이름</h4>
+        <input
+          type={"text"}
+          placeholder="이름 입력"
+          className="info_input"
+          onChange={(event) => setName(event.target.value)}
+        ></input>
+        <h4 className="info_title">생년월일</h4>
+        <input type={"date"} className="info_input"></input>
+        <h4 className="info_title">성별</h4>
+        <div className="info_gender">
+          <button
+            className="info_gender_item"
+            onChange={(event) => setGender(event.target.value)}
+          >
+            남
+          </button>
+          <button
+            className="info_gender_item"
+            onChange={(event) => setGender(event.target.value)}
+          >
+            여
+          </button>
+        </div>
+
+        <h4 className="info_title">키(cm)</h4>
+        <input
+          placeholder="키 입력"
+          type={"number"}
+          value="키 입력"
+          className="info_input"
+          onChange={(event) => setHeight(event.target.value)}
+        ></input>
+        <h4 className="info_title">몸무게(kg)</h4>
+        <input
+          type={"number"}
+          placeholder="몸무게 입력"
+          className="info_input"
+          onChange={(event) => setWeight(event.target.value)}
+        ></input>
+        <h4 className="info_title">과거병력</h4>
+        <input
+          type={"text"}
+          placeholder="과거병력 입력"
+          className="info_input"
+          onChange={(event) => setPastDisease(event.target.value)}
+        ></input>
+        <h4 className="info_title">기타 입력 (예: 식습관)</h4>
+        <input
+          type={"text"}
+          placeholder="기타 입력"
+          className="info_input"
+          onChange={(event) => setExtra(event.target.value)}
+        ></input>
+        <button className="submit_button" onClick={addInfo}>
+          제출
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Info;
