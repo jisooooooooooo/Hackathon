7999adbbde8f7b4df4525e888f1b1206df271109
jisooooooooooo <dlawltn0708@naver.com>
2024-05-04 00:42:36 +0900

    Feat: 헤더 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,13 @@ import styles from "./Header.module.css";
 export const Header = () => {
   return (
     <div className={styles.container}>
-      <div>로고</div>
+      <div className={styles.left}>로고</div>
       <div className={styles.right}>
-        <div>오늘의 식습관</div>
-        <div>캘린더</div>
-        <div>콘텐츠</div>
-        <div>컨설팅</div>
-        <div>마이페이지</div>
+        <div className={styles.rightlist}>오늘의 식습관</div>
+        <div className={styles.rightlist}>캘린더</div>
+        <div className={styles.rightlist}>콘텐츠</div>
+        <div className={styles.rightlist}>컨설팅</div>
+        <div className={styles.rightlist}>마이페이지</div>
       </div>
     </div>
   );
