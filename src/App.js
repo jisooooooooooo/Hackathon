b3736641e77b059542539d3fcaa4a6bd424ef74e
yaho <rlatjwls2923@gmail.com>
2024-05-04 08:18:19 +0900

    feat: childInfoPage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
 import { Header } from "./components/Header";
 import "./reset.css";
+import { useState } from "react";
 // import Chatbot from "./components/chatbot";
 import CalendarPage from "./pages/CalendarPage";
 import ContentsPage from "./pages/ContentPage";
@@ -12,23 +13,37 @@ import MyPage from "./pages/MyPage";
 import NutritionistPage from "./pages/NutritionistPage";
 import Chatbot from "./components/chatbot";
 import Feedback from "./pages/FeedbackPage";
+import ChildInfo from "./pages/ChildInfoPage";
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/today-food" element={<TodayFood />} />
-        <Route path="/calendar" element={<CalendarPage />} />
-        <Route path="/contents" element={<ContentsPage />} />
-        <Route path="/consulting" element={<NutritionistPage />} />
-        <Route path="/my-page" element={<MyPage />} />
-        <Route path="/nutritionist" element={<NutritionistPage />} />
-        <Route path="/chatbot" element={<Chatbot />} />
-        <Route path="/feedback" element={<Feedback />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  const [isFirstVisit, setFirstVisit] = useState(true);
+
+  const handgleFirstVisit = () => {
+    setFirstVisit(false);
+  };
+  if (isFirstVisit) {
+    return (
+      <div>
+        <ChildInfo />
+      </div>
+    );
+  } else {
+    return (
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/today-food" element={<TodayFood />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+          <Route path="/contents" element={<ContentsPage />} />
+          <Route path="/consulting" element={<NutritionistPage />} />
+          <Route path="/my-page" element={<MyPage />} />
+          <Route path="/nutritionist" element={<NutritionistPage />} />
+          <Route path="/chatbot" element={<Chatbot />} />
+          <Route path="/feedback" element={<Feedback />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/components/nutritionist.js
+++ src/components/nutritionist.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "../Styles/NutriPage.css";
+import u1 from "../assets/Ellipse 7.png";
+import u2 from "../assets/Ellipse 8.png";
+import u3 from "../assets/Ellipse 9.png";
+import u4 from "../assets/Ellipse 10.png";
+import u5 from "../assets/Ellipse 13.png";
+import u6 from "../assets/Frame 20.png";
 const Nutritionist = ({ id, name, numConsult, experience }) => {
+  let imageSource;
+  if (id === 1) {
+    imageSource = u1;
+  } else if (id === 2) {
+    imageSource = u2;
+  } else if (id === 3) {
+    imageSource = u3;
+  } else if (id === 4) {
+    imageSource = u4;
+  } else if (id === 5) {
+    imageSource = u5;
+  } else if (id === 6) {
+    imageSource = u6;
+  }
   return (
     <div className="profile">
+      <img src={imageSource} alt={`Nutritionist ${id}`} />
       <div className="nutritionist-name">{name}</div>
       <div className="profile_text">상담 횟수 : {numConsult}</div>
       <div className="profile_text">경력 : {experience}년</div>
