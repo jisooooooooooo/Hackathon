26047d22e1dc8e4957c27c5513697edd28946082
jisooooooooooo <dlawltn0708@naver.com>
2024-05-04 00:23:21 +0900

    Feat: Header 생성

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from "react";
+import styles from "./Header.module.css";
+
 
 export const Header = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>오늘의 식습관</div>
       <div>캘린더</div>
+      <div>콘텐츠</div>
+      <div>컨설팅</div>
+      <div>마이페이지</div>
     </div>
   );
 };
